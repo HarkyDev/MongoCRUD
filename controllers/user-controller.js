c20e133111775d3a96217d80e3ce13fb6edb52aa
Eoin Harkin <eharky@gmail.com>
2022-03-25 22:54:54 +0000

    Added del functionality for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -47,6 +47,7 @@ const userController = {
             res.status(500).json(err);
           });
       },
+      //get single user by their ID
       getUser(req,res){
         User.findOne({ _id: req.params.userId })
       .select('-__v')
@@ -62,6 +63,19 @@ const userController = {
         console.log(err);
         res.status(500).json(err);
       });
+      },
+      deleteUser(req,res){
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            return res.status(404).json({ message: 'No USER with this ID!' });
+          }
+        }).catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+        //DEL is slow and has delay issue when running request but does DEL 
+        
       }
 }
 module.exports = userController;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,11 +2,12 @@ const router = require ('express').Router()
 const { getUsers,
     createUser,
     getUser,
-    updateUser
+    updateUser,
+    deleteUser
  } = require ("../../controllers/user-controller")
 
 router.route("/").get(getUsers).post(createUser)
 
-router.route('/:userId').get(getUser).put(updateUser);
+router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
