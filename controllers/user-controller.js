cc0663f95b66a7337b3d25573dc81891e73c373f
Eoin Harkin <eharky@gmail.com>
2022-03-25 22:39:01 +0000

    added find user and update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -23,6 +23,45 @@ const userController = {
             console.log(err);
             res.status(500).json(err);
           });
+      },
+      //update a user
+      updateUser(req, res) {
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          {
+            $set: req.body,
+          },
+          {
+            runValidators: true,
+            new: true,
+          }
+        )
+          .then((userData) => {
+            if (!userData) {
+              return res.status(404).json({ message: 'No USER with this ID' });
+            }
+            res.json(userData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+      getUser(req,res){
+        User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('friends')
+      .populate('thoughts')
+      .then((userData) => {
+        if (!userData) {
+          return res.status(404).json({ message: 'No USER with this ID' });
+        }
+        res.json(userData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
       }
 }
 module.exports = userController;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,12 @@
 const router = require ('express').Router()
 const { getUsers,
-    createUser
+    createUser,
+    getUser,
+    updateUser
  } = require ("../../controllers/user-controller")
 
 router.route("/").get(getUsers).post(createUser)
+
+router.route('/:userId').get(getUser).put(updateUser);
 
 module.exports = router;
